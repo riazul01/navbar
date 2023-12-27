0ed95c26e3b056e35f3434c8bc4a08a001412d2a
Riazul Islam <riazul5207@gmail.com>
2023-12-27 12:55:16 +0600

    header section completed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+let navLinks = document.querySelectorAll('.navbar > ul > li > a');
+
+for (let i = 0; i < navLinks.length; i ++) {
+    navLinks[i].addEventListener('click', () => {
+        for (let j = 0; j < navLinks.length; j ++) {
+            navLinks[j].classList.remove('active-link');
+        }
+        navLinks[i].classList.add('active-link');
+    });
+}
