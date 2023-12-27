0b2897f73a5c9ca08039222580c988323de1d145
Riazul Islam <riazul5207@gmail.com>
2023-12-27 15:36:10 +0600

    toggle settings functionality added

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ for (let i = 0; i < navLinks.length; i ++) {
         navLinks[i].classList.add('active-link');
     });
 }
+
+
+let settings = document.querySelector('.settings');
+let toggleSettings = document.querySelector('.toggle-settings');
+
+let body = document.querySelector('body');
+
+// toggle switch box
+toggleSettings.addEventListener('click', () => {
+    settings.classList.toggle('show-settings');
+});
+
+// close switch box
+body.addEventListener('click', () => {
+    settings.classList.remove('show-settings');
+});
+
+settings.addEventListener('click', (e) => {
+    e.stopPropagation();
+});
